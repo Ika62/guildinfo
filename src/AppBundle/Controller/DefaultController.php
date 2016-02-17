e54b842dfad432e1f5db14519447b483af789619
Florian NICOLAS <florian.nicolas62@free.fr>
2016-02-18 00:15:43 +0100

    Classes GuildInfo

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Utils\GuildInfo;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,8 +18,11 @@ class DefaultController extends Controller
         $service_url = 'https://eu.api.battle.net/wow/guild/Conseil%20des%20Ombres/Ave%20Tenebrae?locale=fr_FR&apikey=e926wn2xzdpby6qfnfgc3kxa74xpf2qt';
         $result = file_get_contents($service_url);
         $json = json_decode($result, true);
+        $guild = new GuildInfo();
+        $guild->name = $json['name'];
+        $guild->realm = $json['realm'];
+        $guild->level = $json['level'];
 
-
-        return $this->render('default/index.html.twig',array('response' => $json['realm']));
+        return $this->render('default/index.html.twig',array('guild' => $guild ));
     }
 }
---
+++ src/AppBundle/Utils/GuildInfo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Florian NICOLAS
+ * Date: 18/02/2016
+ * Time: 00:06
+ */
+
+namespace AppBundle\Utils;
+
+
+class GuildInfo
+{
+    public $name;
+    public $realm;
+    public $level;
+}
